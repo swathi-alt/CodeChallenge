ee23b939cb4a4c54ea85eb43f8d2ebf4300d8a07
swathi-alt <53555879+swathi-alt@users.noreply.github.com>
2019-08-01 12:49:35 +0530

    Add files via upload
    Hi Sravan,
    
    I committing the Traingle code file.
    
    Regards,
    Swathi

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleProject
+{
+    class Program
+    {
+        // int a = 7, b = 14, c = 16;
+        public static void checktraingle(int a, int b, int c)
+        {
+
+
+
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                Console.WriteLine("The sides given forms a Traingle");
+            }
+
+            else
+                Console.WriteLine("The sides given does not form a Traingle");
+
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+
+            Program.checktraingle(7, 14, 16);
+        }
+    }
+}
